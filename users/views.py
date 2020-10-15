9dcc2af7298d3d97525bd2c55cded57bcf5fca5f
Spenser Tacinelli <spenserdt@gmail.com>
2020-10-15 16:28:16 -0400

    Final Alpha Update

--- users/views.py
+++ users/views.py
@@ -201,6 +201,19 @@ def dashboard(request):
 
     return render(request, 'dashboard.html', context)
 
+def dashboard_blogger_listing_update(request, id=None):
+    listing = BlogListingCreationModel.objects.get(id=id)
+    context = {
+        'listing':listing
+    }
+    if request.method == 'POST':
+        form = BlogListingCreationForm(request.POST, instance=listing)
+        if form.is_valid():
+            form.save()
+
+        return redirect('home')
+    return render(request, 'blog_listing_creation.html', context)
+
 
 def dashboard_send_review(request, id=None):
     # 1. This is for the creator to send over the content they made to the sponsor
