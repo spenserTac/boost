3d2f5d20d5e61bc7873e43c9a17740cb0135874b
Spenser Tacinelli <spenserdt@gmail.com>
2020-11-23 13:43:33 -0500

    Notifications update.

--- users/urls.py
+++ users/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
 
     #path('change-password/', views.change_password, name='change_password'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html', email_template_name='password_reset_email.html'), name='password_reset'),
-    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done'), name='password_reset_done'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('password-reset/confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset/complete', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 
